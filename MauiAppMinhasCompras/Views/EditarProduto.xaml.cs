7cd0e922939280ce6a137007d332196126cc27b2
Cinvipo <cintiavieiras59@gmail.com>
2025-09-07 21:36:09 -0300

    Foi acrescentado o comando de remover produtos.

--- MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,9 +1,28 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
-public partial class EditarProdutos : ContentPage
+public partial class EditarProduto : ContentPage
 {
-	public EditarProdutos()
-	{
-		InitializeComponent();
-	}
+    public EditarProduto()
+    {
+        InitializeComponent();
+    }
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            if (BindingContext is Produto produto_anexado)
+            {
+                await App.Db.Update(produto_anexado);
+                await DisplayAlert("Sucesso!", "Registro Atualizado", "OK");
+                await Navigation.PopAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
